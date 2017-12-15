05f6840c5019911a57fa49ce0164036cef3dad50
daixilong <1113024640@qq.com>
2017-12-15 08:37:35 +0800

    ee

--- spring-boot/src/main/java/com/cn/controller/UserController.java
+++ spring-boot/src/main/java/com/cn/controller/UserController.java
@@ -1,12 +1,29 @@
-package com.cn.controller;
+﻿package com.cn.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PropertySource({"classpath:user.properties"})
 public class UserController {
+		
+		@Value("${user.name}")
+	  private String name;
+	  
+		@Value("${user.hobby}")
+	  private String hoby;
+	
 		@RequestMapping("/home")
 		public String home(){
 			return "首页进入";
 		}
+		
+		
+		@RequestMapping("/user")
+		public String user(){
+			 return name+"的爱好是:"+hoby;
+		}
+		
 }
